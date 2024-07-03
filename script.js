40aeabad8372513a17ee7ef8534be9241a50703a
gabrielyuri2425 <128593358+gabrielyuri2425@users.noreply.github.com>
2024-07-03 11:59:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const caixaPrincipal = document.querySelector(´.caixaPrincipal´);
-const caixaPrincipal = document.querySelector(´.caixaPrincipal´);
-const caixaPrincipal = document.querySelector(´.caixaPrincipal´);
-const caixaPrincipal = document.querySelector(´.caixaPrincipal´);
-const caixaPrincipal = document.querySelector(´.caixaPrincipal´);
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
+    },
+    {
