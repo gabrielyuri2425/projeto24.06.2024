8333de19c658878ea81994e3247a4d165294f82b
gabrielyuri2425 <128593358+gabrielyuri2425@users.noreply.github.com>
2024-08-15 08:36:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,77 +19,37 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Por ser um pouco mais caro tirar a CNH AB, muita pessoas preferem irar a CNH A ou a B pois economizam mais, você acha que isso esta certo? ",
+        enunciado: "Por ser um pouco mais caro tirar a CNH AB, muita pessoas preferem tirar a CNH A ou a B pois economizam mais, você acha que isso certo? ",
         alternativas: [
             {
                 texto: "Sim, pois a CNH AB é mais cara e nem todos tem condições financeiras para pagar. ",
-                afirmacao: "Sim, pois a CNH AB é mais cara e nem todos tem condições financeiras para pagar."
+                afirmacao: "Sim, pois a CNH AB é mais cara e nem todos tem condições financeiras para pagar.",
             },
             {
                 texto: "Não, pois a CNH AB tem mais vantagem por ser mais barato do que tirar a CNH A e B uma por uma",
-                afirmacao: "Não, pois a CNH AB tem mais vantagem por ser mais barato do que tirar a CNH A e B uma por uma"
+                afirmacao: "Não, pois a CNH AB tem mais vantagens, por ser mais barata do que tirar a CNH A e B uma por uma",
             }
         ]
     },
 
     {
-    enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+    enunciado: "Ao completar 18 anos, por um certo preço, as pessoas podem tentar tirar sua primeira carteira de motorista, após passar por provas e testes mentais. Quanto tempo leva para tirar a CNH?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Aproximadamente 90 dias",
+                afirmacao: "Aproximadamente 90 dias",
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
-        ]
-    },
-    {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Aproximadamente 60 dias",
+                afirmacao: "Aproximadamente 60 dias",
             }
         ]
     },
 ];
 
-
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal = "Muitas pessoas tem pressa para completar seus 18 anos de idade para conseguir tirar sua primeira CNH, mas não pensam na mudança total da vida, como tudo vai ser diferente e como muitas responsabilidades vem a tona ao completar 18 anos, por isso, aproveite enquanto a vida é simples e sem as complicações da vida adulta.";
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
@@ -113,13 +73,13 @@ function mostraAlternativas(){
 
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
+    historiaFinal;
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Sem pressa...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
